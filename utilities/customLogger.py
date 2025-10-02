c315c55e04529cd579ce400bd3ca7bb5eccfb294
sainiashu <saini.ashu90@gmail.com>
2025-10-02 12:37:54 -0400

    fixed commoninfo

--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -1,12 +1,43 @@
 import logging
+import os
 
 class LogGen:
     @staticmethod
     def loggen():
-        logger = logging.getLogger()
-        fhandler = logging.FileHandler(filename='.\\Logs\\automation.log', mode='a')
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        fhandler.setFormatter(formatter)
-        logger.addHandler(fhandler)
+        # Correct path to the Logs folder
+        log_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Logs")
+        log_file = os.path.join(log_dir, "automation.log")
+
+        # Create Logs folder if it doesn't exist
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+        # Configure logger
+        logger = logging.getLogger(__name__)
         logger.setLevel(logging.INFO)
+
+        # Avoid duplicate handlers
+        if not logger.handlers:
+            fileHandler = logging.FileHandler(log_file)
+            formatter = logging.Formatter("%(asctime)s: %(levelname)s: %(message)s",
+                                          datefmt="%Y-%m-%d %H:%M:%S")
+            fileHandler.setFormatter(formatter)
+            logger.addHandler(fileHandler)
+
         return logger
+
+
+
+
+# import logging
+#
+# class LogGen:
+#     @staticmethod
+#     def loggen():
+#         logger = logging.getLogger()
+#         fhandler = logging.FileHandler(filename='.\\Logs\\automation.log', mode='a')
+#         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#         fhandler.setFormatter(formatter)
+#         logger.addHandler(fhandler)
+#         logger.setLevel(logging.INFO)
+#         return logger
--- utilities/readProperties.py
+++ utilities/readProperties.py
@@ -1,7 +1,19 @@
 import configparser
-
+import os
 config = configparser.RawConfigParser()
-config.read("./Configuration/config.ini")
+# config.read("./Configuration/config.ini")
+
+# config_path = os.path.join(os.path.dirname(__file__), "Configuration", "config.ini")
+config_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Configuration", "config.ini")
+
+config.read(config_path)
+
+
+# config = configparser.RawConfigParser()
+# # Go up one directory from 'utilities'
+# config_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Configuration", "config.ini")
+read_files = config.read(config_path)
+
 
 class ReadConfig():
     @staticmethod
@@ -18,3 +30,7 @@ class ReadConfig():
     def getPassword():
         password = config.get('common_info', 'password')
         return password
+
+print("Config path:", config_path)
+print("Files read:", read_files)
+print("Sections found:", config.sections())
